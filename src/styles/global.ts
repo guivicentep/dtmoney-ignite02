f736c97658a313efe2c1c0da1a6e3c8bb695f876
guivicentep <guivicente.gvp@hotmail.com>
2021-04-28 00:27:09 -0300

    Finished styling!

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #F0F2F5;
         --red: #E52E4D;
+        --green: #33CC95;
         --blue: #5429CC;
         --blue-light: #6933FF;
         --text-title: #363F5F;
@@ -46,5 +47,42 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+    .react-modal-overlay{
+        background: rgba(0,0,0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+
+        /*Mais para frente iremos adicionar um botão de fechar, para que ele fique junto com nosso modal, devemos colocar essa propriedade abaixo */
+        position: relative;
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.8)
+        }
     }
 `
