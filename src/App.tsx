9dba93ed17308e210f2216249972fa754e644188
guivicentep <guivicente.gvp@hotmail.com>
2021-06-28 13:30:07 -0300

    Criação dos contextos, e finalização da funcionalidade na tela

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Modal from 'react-modal';
 import { useState } from 'react';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionsProvider } from './hooks/useTransactions';
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from './components/NewTransactionModal';
 
@@ -18,13 +19,13 @@ export function App() {
       setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
